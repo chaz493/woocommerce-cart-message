e628613ce41dea6ac547441193a8a9ba6de4fc4d
Charlie J <46576526+chaz493@users.noreply.github.com>
2019-09-12 16:15:15 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 add_action('woocommerce_after_cart_table', 'shipping_disclaimer_message');
 
 function shipping_disclaimer_message( ) {
-echo '<div><span style="font-size: 12px;color: #666767;"><strong>DISCLAIMER:</strong> Shipping costs displayed are an estimate and subject to change based upon weight, quantity of products being ordered, delivery zip code, and freight company selected. Actual shipping cost will be calculated at check-out once order quantities, delivery zip code, and freight company are determined. Please refer any questions regarding shipping costs to Customer Service at 913-401-3330.</span>
+echo '<div><span style="font-size: 12px;color: #666767;">Your message goes here</span>
 </div>'; 
 }
